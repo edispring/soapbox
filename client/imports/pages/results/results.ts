dcaab562f2660629807a467d6b014d51dcb3e362
Edi Spring <edi.spring@lambda-it.ch>
2021-08-27 17:31:33 +0200

    last fix

--- client/imports/pages/results/results.ts
+++ client/imports/pages/results/results.ts
@@ -2,33 +2,33 @@ import { Component, OnInit, OnDestroy } from "@angular/core";
 import { Cars, Runs } from "../../../../imports/collections";
 import { Car, Run } from "../../../../imports/models";
 import { Observable, Subscription } from "rxjs";
-import 'rxjs/add/operator/do';
-import 'rxjs/add/operator/combineLatest';
-import 'rxjs/add/operator/startWith';
+import "rxjs/add/operator/do";
+import "rxjs/add/operator/combineLatest";
+import "rxjs/add/operator/concat";
 import { MeteorObservable, zoneOperator } from "meteor-rxjs";
 import { minBy, groupBy } from "lodash";
 
 @Component({
-  templateUrl:"./results.html"
+  templateUrl: "./results.html",
 })
 export class ResultsPage implements OnInit {
-  result$: Observable<{ [category: string]: any[] }> = Observable.from([]);
+  result$: Observable<any> = Observable.of({ kids: [], bobby: [], adults: [] });
   categories = ["kids", "bobby", "adults"];
 
   ngOnInit() {
     const cars$ = Runs.find({}, { sort: { start: -1 } })
       .combineLatest(Cars.find({ year: 2021 }), (runs, cars) => {
-        console.log("🚀 ~ file: results.ts ~ line 21 ~ ResultsPage ~ .combineLatest ~ runs, cars", runs, cars)
+        console.log("combineLatest ~ runs, cars", runs, cars);
         return groupBy(
           cars
-            .map(car => {
-              const filteredRuns = runs.filter(r => car._id === r.carId);
+            .map((car) => {
+              const filteredRuns = runs.filter((r) => car._id === r.carId);
               const bestRun = minBy(filteredRuns, getDuration);
               car.lastRun = bestRun;
               car.runs = filteredRuns.length;
               return car;
             })
-            .filter(c => c.lastRun && c.lastRun.finished)
+            .filter((c) => c.lastRun && c.lastRun.finished)
             .sort((a, b) => {
               return a.category > b.category ||
                 getDuration(a.lastRun) > getDuration(b.lastRun)
@@ -38,11 +38,11 @@ export class ResultsPage implements OnInit {
           "category"
         );
       })
-      .do(x => {
-        console.log(x, "cars");
+      .do((x) => {
+        console.log("==> cars", x);
       });
 
-    this.result$ = cars$.pipe(zoneOperator()) as any;
+    this.result$ = this.result$.concat(cars$).pipe(zoneOperator()) as any;
   }
 }
 
